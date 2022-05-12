127e54353ef34bd6420b83cbbfa837d9e3eb6977
mavlonergashev <mavlik.exe@gmail.com>
2022-05-13 01:52:42 +0500

    setup cell completed

--- GoogleTranslate/CustomCell.swift
+++ GoogleTranslate/CustomCell.swift
@@ -11,13 +11,38 @@ import SnapKit
 class CustomCell: UITableViewCell {
     
     static let identifierCell = "cell"
-    let clearView = UIView()
-    let containerView = UIView()
-    let txtLabel = UILabel()
+    let clearView: UIView = {
+        let view = UIView()
+        view.backgroundColor = .clear
+        return view
+    }()
+    let containerView: UIView = {
+        let view = UIView()
+        view.backgroundColor = .systemGray5.withAlphaComponent(0.7)
+        return view
+    }()
+    let txtLabel: UILabel = {
+        let lbl = UILabel()
+        lbl.textColor = .black
+        return lbl
+    }()
         
-
-    func setUI() {
+    func configureUI() {
         contentView.addSubview(clearView)
+        clearView.addSubview(containerView)
         
+        clearView.snp.makeConstraints { make in
+            make.edges.equalToSuperview()
+        }
+        containerView.snp.makeConstraints { make in
+            make.left.right.equalToSuperview().inset(16)
+            make.top.bottom.equalToSuperview().inset(8)
+        }
     }
+    
+    func updateCell(text: String) {
+        configureUI()
+        txtLabel.text = text
+    }
+    
 }
